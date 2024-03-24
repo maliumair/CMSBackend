88911508f1b811f68c69da75194194edc4e4b5dd
Muhammad Ali Umair <muhammadaliumair0@gmail.com>
2024-03-25 03:54:52 +0500

    forcing DB change

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(errorHandler)
 // {force: true}
 // only for development
 db.sequelize
-  .sync()
+  .sync({ force: true })
   .then(async () => {
     console.log('Synced db.')
     await db.superAdmin()
